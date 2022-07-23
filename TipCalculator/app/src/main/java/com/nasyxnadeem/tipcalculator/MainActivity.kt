62b66c7f024e5a153faa2f2f4ecdfaf62b444d23
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-07-23 15:23:21 +0530

    add custom input field

--- TipCalculator/app/src/main/java/com/nasyxnadeem/tipcalculator/MainActivity.kt
+++ TipCalculator/app/src/main/java/com/nasyxnadeem/tipcalculator/MainActivity.kt
@@ -3,24 +3,32 @@ package com.nasyxnadeem.tipcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nasyxnadeem.tipcalculator.components.InputField
 import com.nasyxnadeem.tipcalculator.ui.theme.TipCalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp() {
+            MyApp {
                 Text("Hello World")
             }
         }
@@ -29,20 +37,44 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-fun TopHeader() {
+fun MainContent() {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
     Surface(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.padding(2.dp),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
     ) {
         Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-
+            InputField(valueState = totalBillState, labelId = "Enter Bill", enabled = true, isSingleLine = true)
         }
     }
 }
 
-
+//@Preview
+@Composable
+fun TopHeader(totalPerPerson: Double = 134.0) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))), color = Color(0xFFE9D7F7)
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(text = "Total Per Person", style = MaterialTheme.typography.h5)
+            Text(text = "$$total", style = MaterialTheme.typography.h4, fontWeight = FontWeight.ExtraBold)
+        }
+    }
+}
 
 
 @Composable
@@ -55,8 +87,7 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 
-
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TipCalculatorTheme {
