2cd15400b8245c29fabe2f70ac603e20642b5693
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-07-23 14:35:07 +0530

    add app header

--- TipCalculator/app/src/main/java/com/nasyxnadeem/tipcalculator/MainActivity.kt
+++ TipCalculator/app/src/main/java/com/nasyxnadeem/tipcalculator/MainActivity.kt
@@ -3,11 +3,15 @@ package com.nasyxnadeem.tipcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.nasyxnadeem.tipcalculator.ui.theme.TipCalculatorTheme
@@ -16,14 +20,36 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TipCalculatorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                }
+            MyApp() {
+                Text("Hello World")
             }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TopHeader() {
+    Surface(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+        }
+    }
+}
+
+
+
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    TipCalculatorTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            content()
         }
     }
 }
@@ -34,5 +60,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     TipCalculatorTheme {
+        MyApp {
+            Text("Hello again")
+        }
     }
 }
