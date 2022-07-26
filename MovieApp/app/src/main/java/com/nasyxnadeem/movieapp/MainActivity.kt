8a0db4a72ae557362cd65c4078a7a5d5480df641
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-07-26 17:40:32 +0530

    modify .gitignore file

---
+++ MovieApp/app/src/main/java/com/nasyxnadeem/movieapp/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.nasyxnadeem.movieapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nasyxnadeem.movieapp.navigation.MovieNavigation
+import com.nasyxnadeem.movieapp.ui.theme.MovieAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                MovieNavigation()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    MovieAppTheme {
+        content()
+    }
+}
+
+
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyApp {
+        MovieNavigation()
+    }
+}
