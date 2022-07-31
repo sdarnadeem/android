61056b9366d4429ca7706a707581f3487a170635
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-07-31 10:53:00 +0530

    implement retrofit

--- TriviaApp/app/src/main/java/com/nasyxnadeem/triviaapp/di/AppModule.kt
+++ TriviaApp/app/src/main/java/com/nasyxnadeem/triviaapp/di/AppModule.kt
@@ -1,4 +1,34 @@
 package com.nasyxnadeem.triviaapp.di
 
+import com.nasyxnadeem.triviaapp.network.QuestionAPI
+import com.nasyxnadeem.triviaapp.repository.QuestionRepository
+import com.nasyxnadeem.triviaapp.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule  {
+
+    @Singleton
+    @Provides
+    fun provideQuestionRepository(api: QuestionAPI) = QuestionRepository(api)
+
+
+
+    @Singleton
+    @Provides
+    fun provideQuestionApi() : QuestionAPI {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(QuestionAPI::class.java)
+    }
 }
