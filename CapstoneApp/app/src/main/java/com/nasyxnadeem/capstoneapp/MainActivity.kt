273b03bd6965ff178bce2986fc07919902813a1c
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-08-20 21:13:14 +0530

    implement firebase firestore

--- CapstoneApp/app/src/main/java/com/nasyxnadeem/capstoneapp/MainActivity.kt
+++ CapstoneApp/app/src/main/java/com/nasyxnadeem/capstoneapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.nasyxnadeem.capstoneapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,6 +10,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.google.firebase.firestore.FirebaseFirestore
 import com.nasyxnadeem.capstoneapp.ui.theme.CapstoneAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,6 +19,20 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApp() {
                 Text("Hello")
+                val db = FirebaseFirestore.getInstance()
+                val user: MutableMap<String, Any> = HashMap()
+
+                user["firstName"] = "Jeo"
+                user["lastName"] = "James"
+
+                db.collection("users")
+                    .add(user)
+                    .addOnSuccessListener {
+                        Log.d("TAG", "onCreate: $it")
+                    }
+                    .addOnFailureListener {
+                        Log.d("TAG", "onCreate: $it")
+                    }
             }
         }
     }
