f018967cf14a1e868b6a00fc7e3d12f04a6d34c6
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-08-20 16:00:02 +0530

    add google-service.json for firebase

--- CapstoneApp/app/src/main/java/com/nasyxnadeem/capstoneapp/MainActivity.kt
+++ CapstoneApp/app/src/main/java/com/nasyxnadeem/capstoneapp/MainActivity.kt
@@ -6,38 +6,31 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.nasyxnadeem.capstoneapp.ui.theme.CapstoneAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CapstoneAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MyApp()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun MyApp(content: @Composable () -> Unit = {}) {
     CapstoneAppTheme {
-        Greeting("Android")
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            content()
+        }
     }
 }
+
+
