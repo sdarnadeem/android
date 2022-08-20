3ea5e03aa425a85c5495d4ecedd9c29911a9c2c0
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-08-20 12:12:00 +0530

    fix settings view model annotations

--- WeatherForecast/app/src/main/java/com/nasyxnadeem/weatherforecast/screens/setting/SettingScreen.kt
+++ WeatherForecast/app/src/main/java/com/nasyxnadeem/weatherforecast/screens/setting/SettingScreen.kt
@@ -1,10 +1,67 @@
 package com.nasyxnadeem.weatherforecast.screens.setting
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.IconToggleButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.nasyxnadeem.weatherforecast.widgets.WeatherTopBar
 
 @Composable
-fun SettingScreen(navController: NavController) {
-    Text("Setting Screen")
+fun SettingScreen(navController: NavController, settingViewModel: SettingViewModel = hiltViewModel()) {
+    var unitToggleState by remember { mutableStateOf(false) }
+    val measurementUnits = listOf("Imperial (F)", "Metric (C)")
+    var choiceState by remember { mutableStateOf("")}
+    Scaffold(
+        topBar = {
+            WeatherTopBar(
+                title = "Settings",
+                icon = Icons.Default.ArrowBack,
+                isMainScreen = false,
+                navController = navController
+            )
+        }
+    ) { it ->
+        val did = it
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Change units of measurement",
+                    modifier = Modifier.padding(bottom = 15.dp)
+                )
+
+                IconToggleButton(checked = unitToggleState,
+                onCheckedChange = {
+                    unitToggleState = !it
+                    choiceState = if (unitToggleState) {
+                        "Imperial (F)"
+                    } else {
+                        "Metric (C)"
+                    }
+                }
+                    ) {
+
+                }
+            }
+        }
+    }
 }
--- WeatherForecast/app/src/main/java/com/nasyxnadeem/weatherforecast/screens/setting/SettingViewModel.kt
+++ WeatherForecast/app/src/main/java/com/nasyxnadeem/weatherforecast/screens/setting/SettingViewModel.kt
@@ -1,9 +1,11 @@
 package com.nasyxnadeem.weatherforecast.screens.setting
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.nasyxnadeem.weatherforecast.model.Unit
 import com.nasyxnadeem.weatherforecast.repository.WeatherDbRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -11,6 +13,8 @@ import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+
+@HiltViewModel
 class SettingViewModel @Inject constructor(private val repo: WeatherDbRepository) : ViewModel(){
     private val _unitList = MutableStateFlow<List<Unit>>(emptyList())
 
@@ -20,7 +24,12 @@ class SettingViewModel @Inject constructor(private val repo: WeatherDbRepository
         viewModelScope.launch(Dispatchers.IO) {
             repo.getUnits().distinctUntilChanged()
                 .collect {
-
+                    listOfUnits ->
+                    if (listOfUnits.isNullOrEmpty()) {
+                        Log.d("TAG", "EMPTY LIST")
+                    } else {
+                        _unitList.value = listOfUnits
+                    }
                 }
         }
     }
