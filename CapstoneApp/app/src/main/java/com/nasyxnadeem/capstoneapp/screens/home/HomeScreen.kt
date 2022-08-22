5a1ae513c2d394a5ab18ea571ef4f96297299d3e
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-08-22 14:30:47 +0530

    add homescreen button

--- CapstoneApp/app/src/main/java/com/nasyxnadeem/capstoneapp/screens/home/HomeScreen.kt
+++ CapstoneApp/app/src/main/java/com/nasyxnadeem/capstoneapp/screens/home/HomeScreen.kt
@@ -1,10 +1,37 @@
 package com.nasyxnadeem.capstoneapp.screens.home
 
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 @Composable
 fun Home(navController: NavHostController) {
-    Text("I'M HOME, BAHAHA")
+    Scaffold(
+        topBar = {},
+        floatingActionButton = { FABContent() {
+
+        } }
+    ) {
+        val iti = it
+    }
+}
+
+@Composable
+private fun FABContent(
+    onTap: (String) -> Unit = {}
+){
+    FloatingActionButton(
+        onClick = {onTap},
+        shape = RoundedCornerShape(50.dp),
+        backgroundColor = MaterialTheme.colors.background
+    ) {
+        Icon(imageVector = Icons.Default.Add, contentDescription = "Add a book", tint = MaterialTheme.colors.onSecondary)
+    }
 }
--- CapstoneApp/app/src/main/java/com/nasyxnadeem/capstoneapp/screens/login/LoginScreenViewModel.kt
+++ CapstoneApp/app/src/main/java/com/nasyxnadeem/capstoneapp/screens/login/LoginScreenViewModel.kt
@@ -9,6 +9,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
+import com.nasyxnadeem.capstoneapp.model.MUser
 import kotlinx.coroutines.launch
 
 class LoginScreenViewModel : ViewModel() {
@@ -63,10 +64,8 @@ class LoginScreenViewModel : ViewModel() {
 
     private fun createUser(displayName: String?) {
         val userId = auth.currentUser?.uid
-        val user = mutableMapOf<String, Any>()
+        val user = MUser(userId = userId.toString(), displayName = displayName.toString(), avatarUrl = "", quote = "Life's great", profession = "Android Developer", id = null).toMap()
 
-        user["user_id"] = userId.toString()
-        user["display_name"] = displayName.toString()
 
         FirebaseFirestore.getInstance().collection("users").add(user)
     }
