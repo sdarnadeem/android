654b2ea83bef9df2282b42b63cc9cee2679b078f
Nasyx Nadeem <sdarnadeem@gmail.com>
2022-08-29 13:48:40 +0530

    implement update screen navaigation

--- CapstoneApp/app/src/main/java/com/nasyxnadeem/capstoneapp/screens/update/UpdateScreen.kt
+++ CapstoneApp/app/src/main/java/com/nasyxnadeem/capstoneapp/screens/update/UpdateScreen.kt
@@ -1,7 +1,60 @@
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.nasyxnadeem.capstoneapp.data.DataOrException
+import com.nasyxnadeem.capstoneapp.model.MBook
+import com.nasyxnadeem.capstoneapp.screens.home.HomeScreenViewModel
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun UpdateScreen(navController: NavHostController, bookItemId: Any) {
+fun UpdateScreen(
+    navController: NavHostController,
+    bookItemId: String,
+    viewModel: HomeScreenViewModel = hiltViewModel()
+) {
+
+    Scaffold(topBar = {
+        ReaderAppBar(
+            title = "Update Book",
+            icon = Icons.Default.ArrowBack,
+            showProfile = false,
+            navController = navController
+        )
+    }) {
+        val bookInfo = produceState<DataOrException<List<MBook>, Boolean, Exception>>(
+            initialValue = DataOrException(data = emptyList(), true, Exception(""))
+        ) {
+            value = viewModel.data.value
+        }.value
+
+        Surface(modifier = Modifier.fillMaxSize().padding(3.dp)) {
+
+            Column(
+                modifier = Modifier.padding(top = 3.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                if (bookInfo.loading == true) {
+                    LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
+                    bookInfo.loading = false
+                }
+                else {
+                    Text(text = viewModel.data.value.data?.get(0)?.title.toString())
+                }
+            }
+        }
+    }
 
 }
